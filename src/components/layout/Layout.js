8d7843a9e523898eb81dd90cebea992950c5c717
strand93 <strand.93@hotmail.com>
2021-01-25 14:24:04 +0100

    Add favicon

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import favicon from '../../images/favicon.ico';
+import Helmet from 'react-helmet';
 import Footer from './Footer';
 import Header from './Header';
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
       <Header />
       <main className="text-gray-900">{children}</main>
       <Footer />
