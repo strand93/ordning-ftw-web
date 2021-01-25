29961f21961494d9e6fff767fda54ff1141895a8
strand93 <strand.93@hotmail.com>
2021-01-25 12:04:54 +0100

    Update 'About me'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Button from '../components/Button';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import Card from '../components/Card';
 import CustomerCard from '../components/CustomerCard';
 import LabelText from '../components/LabelText';
 import Layout from '../components/layout/Layout';
 import SplitSection from '../components/SplitSection';
-import StatsBox from '../components/StatsBox';
 import customerData from '../data/customer-data';
 import HeroImage from '../svg/HeroImage';
 import SvgCharts from '../svg/SvgCharts';
 import HeaderImage from '../images/header.jpg';
-import ProfileImage from '../images/profil.jpg';
+import ProfileImage from '../images/profile.jpg';
 import BoxesImage from '../images/boxes.jpg';
 import GarageImage from '../images/Garage.jpg';
 import RoomImage from '../images/room.jpg';
@@ -18,7 +18,10 @@ import RoomImage from '../images/room.jpg';
 
 export default () => (
   <Layout>
-    <section className="pt-20 lg:pt-28">
+    {/**
+     * Top
+     */}
+    <section id="top" className="pt-8 md:pt-20 lg:pt-28">
       <div className="container mx-auto px-8 lg:flex justify-center items-center">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl leading-none font-semibold">
@@ -27,12 +30,6 @@ export default () => (
           <p className="text-xl lg:text-2xl mt-6 font-light">
             Älskar kaos. Eller alltså. Att fixa kaoset.
           </p>
-          {/*
-          <p className="mt-8 md:mt-12">
-            <Button size="lg">Get Started</Button>
-          </p>
-          <p className="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>
-          */}
         </div>
         <div className="lg:w-1/3 shadow-2xl pt-6">
         {/* <HeroImage /> */}
@@ -40,6 +37,10 @@ export default () => (
         </div>
       </div>
     </section>
+
+    {/**
+     * Tjänster version 1
+     */}
     <section id="features" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl lg:text-5xl font-semibold">Mina tjänster</h2>
@@ -77,10 +78,13 @@ export default () => (
         </div>
       </div>
     </section>
+
+    {/**
+     * Tjänster version 2
+     */}
     <section id="services2" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold">Mina tjänster</h2>
-     
+        <h2 className="text-3xl lg:text-5xl font-semibold">Mina tjänster</h2> 
     <SplitSection
       id="services"
       primarySlot={
@@ -126,34 +130,30 @@ export default () => (
     />
      </div>
     </section>
-    <section id="about-me" className="text-center">
-    <h2 className="text-3xl lg:text-5xl font-semibold">Om mig</h2>
-        <div className="container md:flex items-center mx-auto">
-          <div className="flex-none justify-self-center w-64 h-64 m-4">
-            <img className="object-scale-down rounded-full" src={ProfileImage} alt="Profile image"/>
-          </div>
-          <div className="flex-1 w-96 mx-4 my-20 py-24 bg-gray-200 rounded-lg text-center shadow-xl">
-            <p className="font-mono text-gray-700">Lite text om mig här ..... 
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
+
+    {/**
+     * Om mig
+     */}
+    <section id="about-me2" className="py-20 lg:pb-40 lg:pt-48">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl lg:text-5xl font-semibold">Om mig</h2>
+        <SplitSection
+      primarySlot={
+        <div className="px-4 py-2 lg:mr-24 xl:mr-32 bg-gray-200 rounded-lg shadow-xl">
+          <p className="mt-8 font-light leading-relaxed font-mono text-gray-700">
+           Lite text om mig här ...
+            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate"   
+          </p>
+        </div>
+      }
+      secondarySlot={<img src={ProfileImage} alt="Profile image" className="rounded-lg"/>}
+    />
       </div>
     </section>
-    {/*<section id="stats" className="py-20 lg:pt-32">
-      <div className="container mx-auto text-center">
-        <LabelText className="text-gray-600">Our customers get results</LabelText>
-        <div className="flex flex-col sm:flex-row mt-8 lg:px-24">
-          <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
-          </div>
-          <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
-          </div>
-          <div className="w-full sm:w-1/3">
-            <StatsBox primaryText="+100%" secondaryText="Stats Information" />
-          </div>
-        </div>
-      </div>
-    </section>*/}
+
+    {/**
+     * Omdömmen
+     */}
     <section id="testimonials" className="py-20 lg:py-40">
       <div className="container mx-auto">
         <LabelText className="mb-8 text-gray-600 text-center">Kund omdömen</LabelText>
@@ -166,6 +166,10 @@ export default () => (
         </div>
       </div>
     </section>
+
+    {/**
+     * Kontakt
+     */}
     <section id="contact" className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
       <h3 className="text-5xl font-semibold">Vill du ha min hjälp?</h3>
       <p className="mt-8 text-xl font-light">
@@ -175,5 +179,11 @@ export default () => (
         organisera@ordning.ftw
       </p>
     </section>
+
+    <div className="container flex justify-center mx-auto px-4 mb-4">
+      <AnchorLink className="px-2 md:px-8" href="#top">
+        <Button className="text-sm">Tillbaka till toppen</Button>
+      </AnchorLink>
+    </div>
   </Layout>
 );
